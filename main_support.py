2722d632e24687e588aad850d6adb27a71014b70
ryku123 <wojtasm@gmail.com>
2016-03-30 18:32:54 +0200

    * InitgPty playground

--- main_support.py
+++ main_support.py
@@ -36,12 +36,63 @@ pmtinf.ReqdExctnDt = datetime.strptime("2016-03-23", '%Y-%m-%d')
 pmtinf.Dbtr = "PayDbtr"
 pmtinf.DbtrAcct = "PayAccount"
 pmtinf.DbtrAgt = "PayDbtrAgent"
-pmtinf.CdtTrfTxInf = ['Dupa']
+pmtinf.CdtTrfTxInf = ['Dupa', 'Sraka']
 
-print "CdtTrfTxInf list? :", type(pmtinf.CdtTrfTxInf)
 
-cstmrCdT = pain.CustomerCreditTransferInitiationV03_CH()
+
+# the default value for minOccurs is 1 -> http://www.w3schools.com/xml/schema_complex_indicators.asp
+
+
+
+
+
+
+### OrgId ###
+
+_orgidVal = pain.OrganisationIdentification4_CH(BICOrBEI="KBBECH22", Othr="SomeOherValue") # dziala to
+# _orgidVal = pain.OrganisationIdentification4_CH()     # takie
+# _orgidVal.BICOrBEI = "KBBECH22321"                    # tez
+# _orgidVal.Othr = "SomeOherValue"                      # dziala
+
+_orgid = pain.Party6Choice_CH(_orgidVal)  #OrgID
+
+### PrvtId ###
+_prvtidVal = pain.PersonIdentification5_CH()  #PrvtId
+_prvtidVal.Othr = "wojtek"
+
+_prvtid = pain.Party6Choice_CH(_prvtidVal)  # super wazne! dodajesz do Id wiec musi to byc insancja klasy podrzednej!
+
+
+
+
+grpHdr.InitgPty = pain.PartyIdentification32_CH_NameAndId(Nm='nazwa', Id=_orgid)
+# grpHdr.InitgPty = pain.PartyIdentification32_CH_NameAndId(Nm='nazwa', Id=_prvtid) # wejdzie ostatni jesli w sekwencji
+
+# albo i tak
+# grpHdr.InitgPty = pain.PartyIdentification32_CH_NameAndId(Nm='nazwa', Id=pain.Party6Choice_CH(pain.OrganisationIdentification4_CH('org_bo_nie_BIC')))
+
+
+# whaaat? sprawdz to
+# grpHdr.InitgPty = pain.PartyIdentification32_CH_NameAndId(Nm='nazwa', Id=pain.Party6Choice_CH(pain.PersonIdentification5_CH('other_bo_nie_date')))
+# or
+# grpHdr.InitgPty = pyxb.BIND('name', pyxb.BIND());
+# grpHdr.InitgPty.Id = pyxb.BIND();
+# grpHdr.InitgPty.Id.OrgId = 'org';
+
+pyxb.utils.domutils.BindingDOMSupport.SetDefaultNamespace("http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd")
+# pyxb.utils.domutils.BindingDOMSupport.SetDefaultNamespace("urn:iso:std:iso:20022:tech:xsd:pain.001.003.03")
+
+# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace(xs.Namespace)
+# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace(pain.Namespace)
+
+# ns2 = pyxb.namespace.NamespaceForURI('urn:iso:std:iso:20022:tech:xsd:pain.001.003.03')
+
+
+
+# print "CdtTrfTxInf list? :", type(pmtinf.CdtTrfTxInf)
+cstmrCdT = pain.CustomerCreditTransferInitiationV03_CH()    # CstmrCdtTrfInitn TAG
 cstmrCdT.GrpHdr = grpHdr
+
 # PmtInf is a multi-valued element so it's a list: append the new value
 print "type(cstmrCdT): ", type(cstmrCdT)
 print "type(cstmrCdT.PmtInf): ", type(cstmrCdT.PmtInf)
@@ -51,30 +102,18 @@ doc = pain.Document()
 doc.CstmrCdtTrfInitn = cstmrCdT
 
 
-# the default value for minOccurs is 1 -> http://www.w3schools.com/xml/schema_complex_indicators.asp
-# Missing piece:
-grpHdr.InitgPty = pain.PartyIdentification32_CH_NameAndId('name',pain.Party6Choice_CH(pain.OrganisationIdentification4_CH('org')))
-# or
-# grpHdr.InitgPty = pyxb.BIND('name', pyxb.BIND());
-# grpHdr.InitgPty.Id = pyxb.BIND();
-# grpHdr.InitgPty.Id.OrgId = 'org';
-
-pyxb.utils.domutils.BindingDOMSupport.SetDefaultNamespace("http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd")
-# pyxb.utils.domutils.BindingDOMSupport.SetDefaultNamespace("urn:iso:std:iso:20022:tech:xsd:pain.001.003.03")
-# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace("urn:iso:std:iso:20022:tech:xsd:pain.001.003.03", prefix=None)
-
-# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace(xs.Namespace)
-# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace(doc.Namespace, 'po')
-
 xxx = doc.toDOM()
-print "xxx", xxx.__class__
-xxx.setAttribute("xmlns", "http://example.net/ns")
 
-print(xxx.toprettyxml())
+# pyxb.utils.domutils.BindingDOMSupport.addXMLNSDeclaration(element='Document', namespace=ns2)
+print "xxx", xxx.__class__
+# xxx.setAttribute("xmlns", "http://example.net/ns")
+# pyxb.utils.domutils.BindingDOMSupport.DeclareNamespace(pain.Namespace, 'xxx')
+
+# print xxx.toprettyxml(encoding='utf-8')
 
 try:
     doc.validateBinding()
-    # print doc.toDOM().toprettyxml(encoding='utf-8')
+    print doc.toDOM().toprettyxml(encoding='utf-8')
 except pyxb.ValidationError as e:
     print(e.details())
 
--- xsd/pain_cutted.py
+++ xsd/pain_cutted.py
@@ -33,7 +33,7 @@ Namespace.configureCategories(['typeBinding', 'elementBinding'])
 
 # custom added 
 # location = 'C:\\Users\\modzelwo\\PycharmProjects\\xsd_cutted_pyxb\\xsd\\pain_cutted.xsd'
-location = '/Users/wojtek/PycharmProjects/xsd_cutted_pyxb/xsd'
+location = '/Users/wojtek/PycharmProjects/xsd_cutted_pyxb1/pain_cutted.xsd'
 
 def CreateFromDocument(xml_text, default_namespace=None, location_base=None):
     """Parse the given XML and use the document element to create a
